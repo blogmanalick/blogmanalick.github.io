7642374388e41cdb5605e915911dcafe9f6556de
Marcus Ortense <marcus.ortense@outlook.com>
2025-10-28 21:43:35 -0300

    feaat: lib/lang

---
+++ src/lib/lang.ts
@@ -0,0 +1,28 @@
+export const LANG_PT = 'pt';
+export const LANG_EN = 'en';
+export const LANG_ES  = 'es';
+export const DEFAULT_LANG = LANG_PT; 
+
+export const LANG_BASE_PATH = {
+    [LANG_PT]: '/',
+    [LANG_EN]: `/${LANG_EN}/`,
+    [LANG_ES]: `/${LANG_ES}/`,
+} as const;
+
+export const LANG_NAME = {
+    [LANG_PT]: 'Português', 
+    [LANG_EN]: 'English',
+    [LANG_ES]: 'Español',
+} as const;
+
+export type LangKey = 
+    | typeof LANG_PT
+    | typeof LANG_EN
+    | typeof LANG_ES;
+
+export function getCurrentLang({ pathname }: Pick<URL, 'pathname'>): LangKey {
+	if (pathname.startsWith(LANG_BASE_PATH[LANG_EN])) return LANG_EN;
+	if (pathname.startsWith(LANG_BASE_PATH[LANG_ES])) return LANG_ES;
+    return DEFAULT_LANG;
+}
+
