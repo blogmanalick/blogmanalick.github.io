dcbe6bafc3512d28a1899331fbbd5d7141525fec
Marcus Ortense <marcus.ortense@outlook.com>
2025-10-29 12:55:36 -0300

    first content release (#1)
    * content: commander-brackets-update-october-2025
    
    * feat: favicon
    
    * refactor: blog lang controller
    
    * feat: better head content + google analytics
    
    * feaat: lib/lang

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mana Lick 👅' as const;
+export const SITE_TITLE = 'Mana Lick' as const;
 export const SITE_DESCRIPTION = 'Welcome to my website!' as const;
 export const SITE_DOMAIN = 'manalick.com' as const;
 export const SITE_URL = `https://${SITE_DOMAIN}` as const;
